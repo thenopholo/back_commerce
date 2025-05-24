756f3b197ca4f7d2979681874624d063e4a45c01
Rodrigo Thenopholo <thenopholo92@icloud.com>
2025-05-24 19:41:56 -0300

    feat: Add product model and his tests

---
+++ internal/entity/product.go
@@ -0,0 +1,59 @@
+package entity
+
+import (
+	"errors"
+	"time"
+
+	"github.com/thenopholo/back_commerce/pkg/entity"
+)
+
+var (
+	ErrIDIsRequired    = errors.New("ID is required")
+	ErrInvalidID       = errors.New("invalid ID")
+	ErrNameIsRequired  = errors.New("name is required")
+	ErrPriceIsRequired = errors.New("price is required")
+	ErrInvalidPrice    = errors.New("invalid price")
+)
+
+type Product struct {
+	ID       entity.ID `json:"id"`
+	Name     string    `json:"name"`
+	Price    float64   `json:"price"`
+	CretedAt time.Time `json:"creted_at"`
+}
+
+func NewProduct(name string, price float64) (*Product, error) {
+	product := &Product{
+		ID:       entity.NewID(),
+		Name:     name,
+		Price:    price,
+		CretedAt: time.Now(),
+	}
+
+	err := product.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
+
+func (p *Product) Validate() error {
+	if p.ID == (entity.ID{}) {
+		return ErrIDIsRequired
+	}
+
+	if p.Name == "" {
+		return ErrNameIsRequired
+	}
+
+	if p.Price == 0.0 {
+		return ErrPriceIsRequired
+	}
+
+	if p.Price < 0.0 {
+		return ErrInvalidPrice
+	}
+
+	return nil
+}
---
+++ internal/entity/product_test.go
@@ -0,0 +1,60 @@
+package entity
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/thenopholo/back_commerce/pkg/entity"
+)
+
+func TestNewProduct(t *testing.T) {
+  product, err := NewProduct("Macbook", 1250.99)
+
+  assert.Nil(t, err)
+  assert.NotNil(t, product)
+  assert.NotEmpty(t, product.ID)
+  assert.Equal(t, "Macbook", product.Name)
+  assert.Equal(t, 1250.99, product.Price)
+  assert.NotEmpty(t, product.CretedAt)
+}
+
+func TestProductWhenNameIsRequired(t *testing.T) {
+  p, err := NewProduct("", 1250.99)
+
+  assert.Nil(t, p)
+  assert.Equal(t, ErrNameIsRequired, err)
+}
+func TestProductWhenPriceIsRequired(t *testing.T) {
+  p, err := NewProduct("Mackbook", 0.0)
+
+  assert.Nil(t, p)
+  assert.Equal(t, ErrPriceIsRequired, err)
+}
+func TestProductWhenPriceIsNotValid(t *testing.T) {
+  p, err := NewProduct("Mackbook", -1)
+
+  assert.Nil(t, p)
+  assert.Equal(t, ErrInvalidPrice, err)
+}
+
+func TestValidate(t *testing.T) {
+  p, err := NewProduct("Macbook", 1250.99)
+
+  assert.Nil(t, err)
+  assert.NotNil(t, p)
+  assert.Nil(t, p.Validate())
+
+}
+
+func TestProduct_ValidateErrIDIsRequired(t *testing.T) {
+    product := &Product{
+        ID:       entity.ID{},
+        Name:     "Test Product",
+        Price:    10.0,
+        CretedAt: time.Now(),
+    }
+
+    err := product.Validate()
+    assert.Equal(t, ErrIDIsRequired, err)
+}
