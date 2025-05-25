38169c2ef7f3e07092be4b691897d045a21c7daa
Rodrigo Thenopholo <thenopholo92@icloud.com>
2025-05-25 11:46:19 -0300

    refactor: changed the repositories package name from db to database to avoid conflicts

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,32 @@
 package main
 
-import "github.com/thenopholo/back_commerce/configs"
+import (
+	"net/http"
+
+	"github.com/thenopholo/back_commerce/configs"
+
+	"github.com/thenopholo/back_commerce/internal/entity"
+	"github.com/thenopholo/back_commerce/internal/infra/database"
+	"github.com/thenopholo/back_commerce/internal/infra/webserver/handler"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
 func main() {
-  config, _ := configs.LoadConfig(".")
-  println(config.DBDriver)
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := gorm.Open(sqlite.Open("teste.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+
+	productDB := database.NewProduct(db)
+	productHandler := handler.NewProductHandler(productDB)
+
+	http.HandleFunc("/products", productHandler.CreateProduct)
+	http.ListenAndServe(":8000", nil)
 }
