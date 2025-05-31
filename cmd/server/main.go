0ffbdb0de070b475e305874a742ae6c0fa7cfbd0
Rodrigo Thenopholo <thenopholo92@icloud.com>
2025-05-31 09:45:06 -0300

    feat: Implement chi router for product endpoints and replace http.HandleFunc

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
 	"github.com/thenopholo/back_commerce/configs"
 
 	"github.com/thenopholo/back_commerce/internal/entity"
@@ -27,6 +29,10 @@ func main() {
 	productDB := database.NewProduct(db)
 	productHandler := handler.NewProductHandler(productDB)
 
-	http.HandleFunc("/products", productHandler.CreateProduct)
-	http.ListenAndServe(":8000", nil)
+	r:= chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Post("/products", productHandler.CreateProduct)
+	r.Get("/products/{id}", productHandler.GetProduct)
+	r.Put("/products/{id}", productHandler.UpdateProduct)
+	http.ListenAndServe(":8000", r)
 }
