7d38c46c9cd574142f5c7a11fb0587549786a04e
Rodrigo Thenopholo <thenopholo92@icloud.com>
2025-06-01 10:49:49 -0300

    refactor: Organize product routes under a dedicated route group with JWT authentication

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/jwtauth"
 	"github.com/thenopholo/back_commerce/configs"
 
 	"github.com/thenopholo/back_commerce/internal/entity"
@@ -32,13 +33,18 @@ func main() {
 	UserDB := database.NewUser(db)
 	userHandler := handler.NewUserHandler(UserDB, config.TokenAuth, config.JWTExpiresIn)
 
-	r:= chi.NewRouter()
+	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Post("/products", productHandler.CreateProduct)
-	r.Get("/products/{id}", productHandler.GetProduct)
-	r.Put("/products/{id}", productHandler.UpdateProduct)
-	r.Delete("/products/{id}", productHandler.DeleteProduct)
-	r.Get("/products", productHandler.GetProducts)
+
+	r.Route("/products", func(r chi.Router) {
+		r.Use(jwtauth.Verifier(config.TokenAuth))
+		r.Use(jwtauth.Authenticator)
+		r.Post("/", productHandler.CreateProduct)
+		r.Get("/", productHandler.GetProducts)
+		r.Get("/{id}", productHandler.GetProduct)
+		r.Put("/{id}", productHandler.UpdateProduct)
+		r.Delete("/{id}", productHandler.DeleteProduct)
+	})
 
 	r.Post("/users", userHandler.CreateUser)
 	r.Post("/users/generate_token", userHandler.GetJWT)
